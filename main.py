e80dadac0d9d100085f66f289822929f7bb8b395
Progasi <ajidan10@gmail.com>
2020-10-09 19:34:39 +0200

    Create main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print('Hello world')
+class web_crawler:
+
+    def get_website(self):
+        pass
+
+    def log_in(self, benutzername, passwort):
+        pass
+
+    def get_page(self):
+        pass
