ce97f58f2a7c98546ee76621b92e5b30ee594335
Progasi <ajidan10@gmail.com>
2020-10-09 19:36:45 +0200

    Change main again to helloWorld file
    Create a Webcrawler file with the class web_crawler

---
+++ Web_crawler.py
@@ -0,0 +1,10 @@
+class web_crawler:
+
+    def get_website(self):
+        pass
+
+    def log_in(self, benutzername, passwort):
+        pass
+
+    def get_page(self):
+        pass
--- main.py
+++ main.py
@@ -1,10 +1,1 @@
-class web_crawler:
-
-    def get_website(self):
-        pass
-
-    def log_in(self, benutzername, passwort):
-        pass
-
-    def get_page(self):
-        pass
+print('Hello world')
